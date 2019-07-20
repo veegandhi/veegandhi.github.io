e914b24e936d275dfc5540e5025d72b769ddaa18
Vandish Gandhi <vandishgandhi@MacBook-Pro.lan>
2019-07-20 11:05:33 +1000

    Updates

---
+++ precache-manifest.1d12615a3c91eabfa22b22344c27deaa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "110367695e96efa01cdb3f8b4e514828",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c12ff19113564fcdfb6",
+    "url": "/static/css/main.4ab97849.chunk.css"
+  },
+  {
+    "revision": "19904ed4fa74387689a0",
+    "url": "/static/js/2.0acb8ac0.chunk.js"
+  },
+  {
+    "revision": "5c12ff19113564fcdfb6",
+    "url": "/static/js/main.4dfb8aad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
