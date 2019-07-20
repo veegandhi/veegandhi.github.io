745cb127243ded585f730fa6482d33b7dcf9251f
CircleCI <builds@circleci.com>
2019-07-20 07:10:04 +0000

    release Sat Jul 20 07:10:04 UTC 2019

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ab019db4bb9ffb04911d8e21c8ff61f.js"
+  "/precache-manifest.ba5a80dbe66ae7c97505040a02441e68.js"
 );
 
 self.addEventListener('message', (event) => {
